038647232f088dbea51b91814162bb3a61ae4109
AVAndrianov <a.v.andrianov33@gmail.com>
2025-07-21 20:38:12 +0300

    first commit

--- src/main/java/com/example/camundadebitapp/service/StockExchangeService.java
+++ src/main/java/com/example/camundadebitapp/service/StockExchangeService.java
@@ -1,5 +1,6 @@
 package com.example.camundadebitapp.service;
 
+
 import com.example.camundadebitapp.model.JsonMapper;
 import com.example.camundadebitapp.util.URLDownloader;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -16,21 +17,39 @@ import java.util.stream.Collectors;
 @Service("stockExchangeService")
 @Slf4j
 public class StockExchangeService implements JavaDelegate {
+
     @Autowired
     private String downloadUrl;
 
     @Override
-    public void execute(DelegateExecution execution) throws Exception {
-        String message = URLDownloader.download(downloadUrl);
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonMapper.Response elements = objectMapper.readValue(message, JsonMapper.Response.class);
-        elements.records = elements.records.stream().filter(
-                i -> Objects.equals(i.BlockDate, "")
-                        && i.INN.startsWith("77")
-                        && !i.Name.contains("ИП")
-        ).collect(Collectors.toList());
-        execution.setVariable("filterMessage", toJson(elements));
-        execution.setVariable("count", elements.records.size());
+    public void execute(DelegateExecution execution) {
+        try {
+            String message = URLDownloader.download(downloadUrl);
+            if (message == null || message.isEmpty()) {
+                log.warn("URLDownloader вернул пустую строку.  Активируем таймер.");
+                execution.setVariable("downloadSuccessful", false);
+                return;
+            }
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonMapper.Response elements = objectMapper.readValue(message, JsonMapper.Response.class);
+            elements.records = elements.records.stream().filter(
+                            i -> Objects.equals(i.BlockDate, "")
+                                    && i.INN.startsWith("77")
+                                    && !i.Name.contains("ИП")
+                    )
+                    .collect(Collectors.toList());
+            execution.setVariable("filterMessage", toJson(elements));
+            execution.setVariable("count", elements.records.size());
+            execution.setVariable("downloadSuccessful", true);
+
+            log.info("Данные успешно загружены и обработаны.");
+
+        } catch (Exception e) {
+            log.error("Ошибка при загрузке данных: " + e.getMessage(), e);
+            execution.setVariable("downloadSuccessful", false);
+            return;
+        }
     }
 
     public static String toJson(JsonMapper.Response response) throws JsonProcessingException {
