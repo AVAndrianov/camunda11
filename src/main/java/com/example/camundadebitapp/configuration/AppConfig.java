ad06dd5b34273cbe740f533850e681ce5cfd6b1d
AVAndrianov <a.v.andrianov33@gmail.com>
2025-07-21 20:46:26 +0300

    first commit

---
+++ src/main/java/com/example/camundadebitapp/configuration/AppConfig.java
@@ -0,0 +1,17 @@
+package com.example.camundadebitapp.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${download.url}")
+    private String downloadUrl;
+
+    @Bean
+    public String downloadUrl() {
+        return downloadUrl;
+    }
+}
