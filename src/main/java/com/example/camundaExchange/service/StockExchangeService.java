9a240dd15cc356a652b85302325dbdbec8903d2c
AVAndrianov <a.v.andrianov33@gmail.com>
2025-07-22 16:53:10 +0300

    first commit

--- src/main/java/com/example/camundaExchange/service/StockExchangeService.java
+++ src/main/java/com/example/camundaExchange/service/StockExchangeService.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
  */
 @Service("stockExchangeService")
 @Slf4j
-public class StockExchangeService implements StockExchangeProcessor {
+public class StockExchangeService implements JavaDelegate {
 
     /**
      * URL для загрузки данных с фондовой биржи.
